92761f864b31c968c87ffa02df4ee38b3d9aad7d
Dan <dpford@gmail.com>
2013-08-05 20:47:40 -0400

    first commit

---
+++ doctor.py
@@ -0,0 +1,265 @@
+# Dr. Mario
+# By Dan Ford dpford@gmail.com
+# http://github.com/dpford
+
+import random
+import time
+import pygame
+import sys
+
+from pygame.locals import *
+
+FPS = 25
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+BOXSIZE = 20
+BOARDWIDTH = 10
+BOARDHEIGHT = 20
+
+MOVESIDEWAYSFREQ = 0.15
+MOVEDOWNFREQ = 0.1
+
+XMARGIN = int((WINDOWWIDTH - BOARDWIDTH * BOXSIZE) / 2)
+TOPMARGIN = WINDOWHEIGHT - (BOARDHEIGHT * BOXSIZE) -5
+
+RED = (155, 0, 0)
+LIGHTRED = (175, 20, 20)
+BLUE = (0, 0, 155)
+LIGHTBLUE = (20, 20, 175)
+YELLOW = (155, 155, 0)
+LIGHTYELLOW = (175, 175, 20)
+
+COLORS = (RED, BLUE, YELLOW)
+LIGHTCOLORS = (LIGHTRED, LIGHTBLUE, LIGHTYELLOW)
+assert len(COLORS) == len(LIGHTCOLORS)
+
+TEMPLATEWIDTH = 2
+TEMPLATEHEIGHT = 2
+BLANK = '.'
+
+ORIENTATION = [['..',
+				   'AB'],
+				  ['.B',
+				   '.A'],
+				  ['..',
+				   'BA'],
+				  ['.A',
+				   '.B']]
+
+def main():
+	global FPSCLOCK, DISPLAYSURF, BASICFONT, BIGFONT
+	pygame.init()
+	FPSCLOCK = pygame.time.Clock()
+	DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+	BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+	BIGFONT = pygame.font.Font('freesansbold.ttf', 100)
+	pygame.display.set_caption('Dr. Mario')
+
+	showTextScreen('Dr. Mario')
+	while True: #game loop
+		pygame.mixer.music.load('doctor_music/03_-_Dr._Mario_-_NES_-_Fever.ogg')
+		pygame.mixer.music.play(-1, 0.0)
+		runGame()
+		pygame.mixer.music.stop()
+		showTextScreen('Game Over')
+		pygame.mixer.music.load('doctor_music/08_-_Dr._Mario_-_NES_-_VS_Game_Over.ogg')
+		pygame.mixer.music.play(3, 0.0)
+
+def runGame():
+	board = getInitialBoard()
+	lastMoveDownTime = time.time()
+	lastMoveSidewaysTime = time.time()
+	lastFallTime = time.time()
+	movingDown = False
+	movingLeft = False
+	movingRight = False
+	score = 0
+	level, fallFreq = calculateLevelAndFallFreq(score)
+
+	fallingPiece = getNewPiece()
+	nextPiece = getNewPiece()
+
+	while True: #main game loop
+		if fallingPiece == None:
+			# No falling pill in play, so put one at the top
+			fallingPiece = nextPiece
+			nextPiece = getNewPiece()
+			lastFallTime = time.time() #reset lastFallTime
+
+			if not isValidPosition(board, fallingPiece):
+				return # can't find a new pill, so you lose!
+
+		checkForQuit()
+		for event in pygame.event.get(): #event handling loop
+			if event.type == KEYUP:
+				if (event.key == K_p): # pause game
+					DISPLAYSURF.fill(BGCOLOR)
+					pygame.mixer.music.stop()
+					showTextScreen('Paused') #until a key is pressed
+					pygame.mixer.music.play(-1 0.0)
+					lastFallTime = time.time()
+					lastMoveDownTime = time.time()
+					lastMoveSidewaysTime = time.time()
+				elif (event.key == K_LEFT or event.key == K_a):
+					movingLeft = False
+				elif (event.key == K_RIGHT or event.key == K_d):
+					movingRight = False
+				elif (event.key == K_DOWN or event.key == K_s):
+					movingDown = False
+
+			elif event.type == KEYDOWN:
+				# moving block sideways
+				if (event.key == K_LEFT or event.key == K_a) and \
+				 isValidPosition(board, fallingPiece, adjX=-1):
+				 	fallingPiece['x'] -= 1
+				 	movingLeft = True
+				 	movingRight = False
+				 	lastMoveSidewaysTime = time.time()
+
+				 elif (event.key == K_RIGHT or event.key == K_d) and \
+				 isValidPosition(board, fallingPiece, adjX=1):
+				 	fallingPiece['x'] += 1
+				 	movingRight = True
+				 	movingLeft = True
+				 	lastMoveSidewaysTime = time.time()
+
+				 #rotating the pill (if there's room)
+				elif (event.key == K_UP or event.key == K_w):
+					fallingPiece['rotation'] = (fallingPiece['rotation'] + 1) % 4
+					if not isValidPosition(board, fallingPiece):
+						fallingPiece['rotation'] = (fallingPiece['rotation'] - 1) % 4
+				elif (event.key ==K_q): #other direction
+					fallingPiece['rotation'] = (fallingPiece['rotation'] - 1) % 4
+					if not isValidPosition(board, fallingPiece):
+						fallingPiece['rotation'] = (fallingPiece['rotation'] + 1) % 4
+				# drop pill faster with down key
+				elif (event.key == K_DOWN or event.key == K_s):
+					movingDown = True
+					if isValidPosition(board, fallingPiece, adjY=1):
+						fallingPiece['y'] += 1
+					lastMoveDownTime = time.time()
+
+		if (movingLeft or movingRight) and time.time() - lastMoveSidewaysTime > MOVESIDEWAYSFREQ:
+			if movingLeft and isValidPosition(board, fallingPiece, adjX=-1):
+				fallingPiece['x'] -= 1
+			elif movingRight and isValidPosition(board, fallingPiece, adjX=1):
+				fallingPiece['x'] += 1
+			lastMoveSidewaysTime = time.time()
+
+		if movingDown and time.time() - lastMoveDownTime > MOVEDOWNFREQ and isValidPosition(board, fallingPiece, adjY=1):
+			fallingPiece['y'] += 1
+			lastMoveDownTime = time.time()
+
+		#let the pill fall down on its own
+		if time.time() - lastFallTime > fallFreq:
+			# see if pill has landed
+			if not isValidPosition(board, fallingPiece, adjY=1):
+				# it's landed, add to board
+				addToBoard(board, fallingPiece)
+				score += removeCompletes(board)
+				level, fallFreq = calculateLevelAndFallFreq(score)
+				fallingPiece = None
+			else:
+				fallingPiece['y'] += 1
+				lastFallTime = time.time()
+
+		# draw everything on the screen
+		DISPLAYSURF.fill(BGCOLOR)
+		drawBoard(board)
+		drawStatus(score, level)
+		drawNextPiece(nextPiece)
+		if fallingPiece != None:
+			drawPiece(fallingPiece)
+
+		pygame.display.update()
+		FPSCLOCK.tick(FPS)
+
+def makeTextObjs(text, font, color):
+	surf = font.render(text, True, color)
+	return surf, surf.get_rect()
+
+def terminate():
+	pygame.quit()
+	sys.exit()
+
+def checkForKeyPress():
+	# go through event queue looking for KEYUP
+	# Grab KEYDOWN events to remove them from the queue
+	checkForQuit()
+
+	for event in pygame.event.get([KEYDOWN, KEYUP]):
+		if event.type == KEYDOWN:
+			continue
+		return event.key
+	return None
+
+def showTextScreen(text):
+	# Displays large text in center of screen until a key is pressed
+	# Draw the text drop shadow
+	titleSurf, titleRect = makeTextObjs(text, BIGFONT, TEXTSHADOWCOLOR)
+	titleRect.center = (int(WINDOWWIDTH / 2), int(WINDOWHEIGHT / 2))
+	DISPLAYSURF.blit(titleSurf, titleRect)
+
+	# Draw the text
+	titleSurf, titleRect = makeTextObjs(text, BIGFONT, TEXTCOLOR)
+	titleRect.center = (int(WINDOWWIDTH / 2) - 3, int(WINDOWHEIGHT / 2) - 3)
+
+	DISPLAYSURF.blit(titleSurf, titleRect)
+
+	# Draw the additional "Press a key to play." text.
+	pressKeySurf, pressKeyRect = makeTextObjs('Press a key to play.', BASICFONT, TEXTCOLOR)
+	pressKeyRect.center = (int(WINDOWWIDTH / 2), int(WINDOWHEIGHT / 2) + 100)
+
+	DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
+
+	while checkForKeyPress() == None:
+		pygame.display.update()
+		FPSCLOCK.tick()
+
+def checkForQuit():
+	for event in pygame.event.get(QUIT): #get all QUIT events
+		terminate() #terminate if any QUIT event present
+	for event in pygame.event.get(KEYUP): # get all KEYUP events
+		if event.key == K_ESCAPE:
+			terminate() #terminate if KEYUP was esc key
+		pygame.event.post(event) # put other KEYUP event objects back
+
+def calculateLevelAndFallFreq(score):
+	level = int(score / 10) + 1
+	fallFreq = 0.27 - (level * 0.02)
+	return level.fallFreq
+
+def getNewPiece():
+	# return a new pill with random colors
+	newPiece = {'A': random.randit(0, len(COLORS)-1),
+				'B': random.randit(0, len(COLORS)-1),
+				'rotation': 0,
+				'x': int(BOARDWIDTH / 2) - int(TEMPLATEWIDTH / 2),
+				'y': 0}
+
+
+def addToBoard(board, piece):
+	# fill in the board based on piece's location
+
+def getBlankBoard():
+
+def isOnBoard(x, y):
+
+def isValidPosition(board, piece, adjX=0, adjY=0):
+
+def isCompleteSet(board, fallingPiece):
+
+def removeCompleteSet(board):
+
+def convertToPixelCoords(boxx, boxy):
+
+def drawBox(boxx, boxy, color, pixelx=None, pixely=None):
+
+def drawStatus(score, level):
+
+def drawPiece(piece, pixelx=None, pixely=None):
+
+def drawNextPiece(piece):
+
+if __name__ == '__main__':
+	main()
